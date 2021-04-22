ef16c0ca8d3f68f9d31825095b95e7bd273a352f
Fish <fishhuang068@gmail.com>
2021-04-23 00:20:24 +0800

    add commit

--- guessnumber.py
+++ guessnumber.py
@@ -25,18 +25,20 @@
 # 老師解法一(basic version)：
 #============================
 import random 
-
+count = 0
 r = random.randint(1,100)
 while True:
+    count += 1 #count = count +1 
     num = input('請猜數字: ')
     num = int(num)
     if num == r:
         print('你猜中了!')
+        print('這是你猜的第', count, '次')
         break
     elif num > r:
         print('比答案大')
     elif num < r:
         print('比答案小')
-    
+    print('這是你猜的第', count, '次')
 
 
